52332df44616d7a13a912e77d0c3b7dacdcb3611
David <dvdcharles222@gmail.com>
2024-01-21 22:03:47 -0300

    Praticando classes

--- aula81 - biblioteca_math#p2/aula81.js
+++ aula81 - biblioteca_math#p2/aula81.js
@@ -3,13 +3,20 @@ const olhos=[...document.getElementsByClassName('olho')]
 let posx_mouse=0
 let posy_mouse=0
 
-window.addEventListener('mousemove',(evt)=>{
-    posx_mouse=evt.clientX
-    posy_mouse=evt.clientY
+//pegando cordenada do mouse
+window.addEventListener('mousemove',(evt)=>{//Evento na pagina 
+    posx_mouse=evt.clientX//pegando posição x do mouse em cada evento
+    posy_mouse=evt.clientY//pegando posição y do mouse em cada evento
 
+    //
     const rot=Math.atan2(posy_mouse,posx_mouse)*180/Math.PI
 
-    olhos.forEach((o)=>{
+    olhos.forEach((o)=>{//percorrendo os dois olhos
         o.style.transform='rotate('+rot+'deg)'
     })
 })
+
+//ROTATE -> precisa passar um angulo, em deg 'degraus'
+//STYLE.TRANSFORM - é do css
+//MATH.atan2 -> retorna o arco tangente de 2 coeficientes -> em radianos
+//Radianos para graus -> multiplico o radiano por 180 e divido por PI
